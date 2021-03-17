db47e4d54701b4612718cfa8d6bf7915bc021fba
sjpaik <glterran19@gmail.com>
2021-03-17 15:04:19 +0900

    3.0 Update

--- src/main/java/com/east/framework/core/persistence/dao/page/PageInfo.java
+++ src/main/java/com/east/framework/core/persistence/dao/page/PageInfo.java
@@ -3,11 +3,14 @@ package com.east.framework.core.persistence.dao.page;
 import java.io.Serializable;
 import java.util.List;
 
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.east.framework.core.util.properties.PropertiesMsgUtil;
+
 /**
- * Page Info.
- *
+ * Page Info
  * Updated on : 2015-09-25 Updated by : love.
- *
  * @param <T>
  */
 public class PageInfo<T> implements Serializable {
@@ -15,29 +18,22 @@ public class PageInfo<T> implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 생성자.
+	 * 생성자
 	 */
-	public PageInfo() {
-	}
+	public PageInfo() {}
 
 	/**
-	 * @param data
-	 *            data
+	 * @param data data
 	 */
 	public PageInfo(List<T> data) {
 		this.data = data;
 	}
 
 	/**
-	 *
-	 * @param pageIndex
-	 *            - 현재 페이지 번호
-	 * @param pageSize
-	 *            - 한 페이지 조회 수(pageSize)
-	 * @param totalCount
-	 *            - 총 페이지 수
-	 * @param data
-	 *            data
+	 * @param pageIndex - 현재 페이지 번호
+	 * @param pageSize - 한 페이지 조회 수(pageSize)
+	 * @param totalCount - 총 페이지 수
+	 * @param data data
 	 */
 	public PageInfo(int pageIndex, int pageSize, int totalCount, List<T> data) {
 		this.pageIndex = pageIndex;
@@ -47,47 +43,39 @@ public class PageInfo<T> implements Serializable {
 	}
 
 	private int totalCount;
-
 	private int pageIndex;
-
 	private int pageSize;
-
 	private List<T> data;
 
-	/**
-	 * get 현재 페이지 번호.
-	 *
-	 * @return pageIndex
-	 */
-	public int getPageIndex() {
-		return this.pageIndex;
-	}
-
-	/**
-	 * get 한 페이지 조회 수.
-	 *
-	 * @return pageSize
-	 */
-	public int getPageSize() {
-		return this.pageSize;
-	}
-
-	/**
-	 * get 총 페이지 수.
-	 *
-	 * @return totalCount
-	 */
 	public int getTotalCount() {
-		return this.totalCount;
+		return totalCount;
 	}
 
-	/**
-	 * List<데이터 테이블>.
-	 *
-	 * @return List<T>
-	 */
+	public int getPageIndex() {
+		return pageIndex;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
 	public List<T> getData() {
-		return this.data;
+		return data;
 	}
 
+	public String getPaging(String templateId) {
+		PagingRenderer pagingRenderer = new PagingRenderer(templateId, ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest());
+		pagingRenderer.doWritePageIndex(this.totalCount, this.pageIndex, this.pageSize, "goPage");
+		return pagingRenderer.getJwt();
+	}
+	
+	public String getPagingHeader(String templateId) {
+		int maxPage = this.totalCount / this.pageSize;
+        if (totalCount % pageSize > 0) {
+            ++maxPage;
+        }
+		
+		return PropertiesMsgUtil.getMessage(templateId + ".headerIndex", new Object[] { this.totalCount, this.pageIndex, maxPage }, ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest());
+	}
+	
 }
